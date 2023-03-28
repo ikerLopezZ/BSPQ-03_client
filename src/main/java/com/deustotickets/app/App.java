bd90ee8b03ccc1f4e06eb031b5fd6252131d4ec6
Iker López <iker.lopezar@opendeusto.es>
2023-03-28 21:13:24 +0200

    LookAndFeel añadido a App.java
    Co-Authored-By: Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>

--- src/main/java/com/deustotickets/app/App.java
+++ src/main/java/com/deustotickets/app/App.java
@@ -1,16 +1,24 @@
 package com.deustotickets.app;
 
+import javax.swing.UIManager;
+
 import com.deustotickets.domain.TipoUsuario;
+
+import com.deustotickets.gui.LoginWindow;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Resource res = new Resource("127.0.0.1", "8080");
-        res.registerUser("Usuario1", "u1@gmail.com", "u1", TipoUsuario.CLIENTE);
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception ignored) {
+		}
+		Resource res = new Resource("127.0.0.1", "8080");
+		res.registerUser("Gestor1", "gestor1@deustotickets.es", "g1", TipoUsuario.GESTOR);
+		LoginWindow win = new LoginWindow();
+		win.initialize();
+	}
 }
