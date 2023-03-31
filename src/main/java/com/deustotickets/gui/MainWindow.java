6c6a20599cf48fbdb927ba76dc1a38e2a363e518
MarkelContreras <markel.contreras@opendeusto.es>
2023-03-31 08:44:50 +0200

    Creación del menu
    Creación de los botones en la ventana con una barra de menu

--- src/main/java/com/deustotickets/gui/MainWindow.java
+++ src/main/java/com/deustotickets/gui/MainWindow.java
@@ -2,8 +2,13 @@ package com.deustotickets.gui;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 import com.deustotickets.domain.Usuario;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 /**
  * 
@@ -14,8 +19,8 @@ public class MainWindow {
 	private static JFrame frmMain;
 	private static ImageIcon logo = new ImageIcon("images/deustotickets_logo.png");
 	public static Usuario logged = null;
-
 	/**
+	 * @wbp.parser.entryPoint
 	 * 
 	 */
 	public static void initialize() {
@@ -27,6 +32,22 @@ public class MainWindow {
 		frmMain.setIconImage(logo.getImage());
 		frmMain.getContentPane().setLayout(null);
 		
+		JPanel panel = new JPanel();
+		panel.setBounds(230, 72, 405, 538);
+		frmMain.getContentPane().add(panel);
+		
+		JMenu mnNewMenu = new JMenu("Cuenta");
+		mnNewMenu.setBounds(0, 0, 111, 24);
+		frmMain.getContentPane().add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Cerrar Sesion");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Cambiar contraseña");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Cambiar usuario");
+		mnNewMenu.add(mntmNewMenuItem_2);
 		frmMain.setVisible(true);
 	}
 }
