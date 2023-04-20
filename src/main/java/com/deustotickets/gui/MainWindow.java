eaeaab812bc948cc55671a10e4391ce2dd76ba5c
Cristina Alonso <cris.alonso@opendeusto.es>
2023-04-20 16:37:35 +0200

    Cambios ventana principal y perfil
    Cambios en la ventana principal y en la ventana del perfil del usuario

--- src/main/java/com/deustotickets/gui/MainWindow.java
+++ src/main/java/com/deustotickets/gui/MainWindow.java
@@ -29,6 +29,7 @@ public class MainWindow {
 	private static JFrame frmMain;
 	private static ImageIcon logo = new ImageIcon("images/deustotickets_logo.png");
 	private static ImageIcon logoPerfil = new ImageIcon("images/logoPerfil.png");
+	private static ImageIcon logoPerfilPeque = new ImageIcon("images/logoPerfilPeque.png");
 	private static ImageIcon inicio = new ImageIcon("images/inicio.png");
 	public static Usuario logged = null;
 
@@ -38,6 +39,7 @@ public class MainWindow {
 	 */
 	public static void initialize() {
 		frmMain = new JFrame();
+		frmMain.getContentPane().setBackground(new Color(255, 255, 255));
 		frmMain.setTitle("DeustoTickets - Ventana principal");
 		frmMain.setBounds(100, 100, 800, 600);
 		frmMain.setResizable(false);
@@ -52,136 +54,28 @@ public class MainWindow {
 		panelPantalla.setBackground(Color.WHITE);
 		panelPantalla.setLayout(null);
 
-		JLabel lblTituloPerfil = new JLabel("PERFIL DEL USUARIO");
-		lblTituloPerfil.setBounds(0, 0, 686, 27);
-		lblTituloPerfil.setFont(new Font("Footlight MT Light", Font.BOLD, 28));
-		lblTituloPerfil.setHorizontalAlignment(SwingConstants.CENTER);
-		panelPantalla.add(lblTituloPerfil);
+		JLabel lblTitulo = new JLabel("DEUSTO TICKETS");
+		lblTitulo.setBounds(0, 0, 656, 59);
+		lblTitulo.setFont(new Font("Footlight MT Light", Font.BOLD, 28));
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		panelPantalla.add(lblTitulo);
 
 		JPanel panelDatos = new JPanel();
-		panelDatos.setBounds(0, 27, 686, 468);
+		panelDatos.setBounds(0, 57, 686, 438);
 		panelDatos.setBackground(new Color(150, 177, 216));
 		panelPantalla.add(panelDatos);
 		panelDatos.setLayout(null);
 
-		JLabel lblNombreUsuario = new JLabel("NOMBRE:");
-		lblNombreUsuario.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNombreUsuario.setForeground(new Color(255, 255, 255));
-		lblNombreUsuario.setBackground(new Color(183, 183, 183));
-		lblNombreUsuario.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNombreUsuario.setBounds(89, 160, 199, 60);
-		panelDatos.add(lblNombreUsuario);
-
-//		Resource.loginUser(textFieldEmail.getText());
-
-		JLabel lblNom = new JLabel(logged.getNombreApellidos());
-		lblNom.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNom.setForeground(new Color(255, 255, 255));
-		lblNom.setBounds(324, 181, 289, 20);
-		panelDatos.add(lblNom);
-
-		JLabel lblMail = new JLabel("CORREO ELECTRÓNICO:");
-		lblMail.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblMail.setForeground(new Color(255, 255, 255));
-		lblMail.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblMail.setBounds(57, 210, 231, 60);
-		panelDatos.add(lblMail);
-
-		JLabel lblCorreo = new JLabel(logged.getEmail());
-		lblCorreo.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblCorreo.setForeground(new Color(255, 255, 255));
-		lblCorreo.setBounds(323, 233, 290, 20);
-		panelDatos.add(lblCorreo);
-
-		JLabel lblConstrasena = new JLabel("CONTRASENA:");
-		lblConstrasena.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblConstrasena.setForeground(new Color(255, 255, 255));
-		lblConstrasena.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblConstrasena.setBounds(132, 269, 156, 60);
-		panelDatos.add(lblConstrasena);
-
-		JLabel lblContra = new JLabel(logged.getPassword());
-		lblContra.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblContra.setForeground(new Color(255, 255, 255));
-		lblContra.setBounds(324, 290, 289, 20);
-		panelDatos.add(lblContra);
-
-		JLabel lblDatos = new JLabel("DATOS DEL USUARIO:");
-		lblDatos.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDatos.setForeground(Color.WHITE);
-		lblDatos.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblDatos.setBackground(new Color(183, 183, 183));
-		lblDatos.setBounds(204, 90, 199, 60);
-		panelDatos.add(lblDatos);
-
 		JPanel panelControlPerfil = new JPanel();
 		panelControlPerfil.setBounds(0, 495, 686, 68);
 		panelPantalla.add(panelControlPerfil);
-
-		JButton btnCambiarNombre = new JButton("CAMBIAR NOMBRE DE USUARIO");
-		btnCambiarNombre.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Nombre de usuario antiguo: " + logged.getNombreApellidos());
-				changeUsername();
-				System.out.println("Nombre de usuario nuevo: " + logged.getNombreApellidos());
-				lblNom.setText(logged.getNombreApellidos());
-			}
-		});
 		panelControlPerfil.setLayout(new GridLayout(2, 2, 0, 0));
-		btnCambiarNombre.setFont(new Font("Footlight MT Light", Font.BOLD, 12));
-		panelControlPerfil.add(btnCambiarNombre);
-
-		JButton btnCambiarContrasena = new JButton("CAMBIAR CONTRASENA");
-		btnCambiarContrasena.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Contrasenya antigua: " + logged.getPassword());
-				changePassword();
-				System.out.println("Contrasenya nueva: " + logged.getPassword());
-				lblContra.setText(logged.getPassword());
-			}
-		});
-		btnCambiarContrasena.setFont(new Font("Footlight MT Light", Font.BOLD, 12));
-		panelControlPerfil.add(btnCambiarContrasena);
-
-		JButton btnBorrarCuenta = new JButton("BORRAR CUENTA");
-		btnBorrarCuenta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Resource.deleteAccount(logged.getEmail());
-				frmMain.dispose();
-				LoginWindow lw = new LoginWindow();
-				lw.initialize();
-			}
-		});
-		
-		btnBorrarCuenta.setFont(new Font("Footlight MT Light", Font.BOLD, 12));
-		panelControlPerfil.add(btnBorrarCuenta);
-		
-		JButton btnCerrarSesion = new JButton("CERRAR SESIÓN");
-		btnCerrarSesion.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Resource.closeSession();
-				frmMain.dispose();
-				LoginWindow lw = new LoginWindow();
-				lw.initialize();
-			}
-		});
-		panelControlPerfil.add(btnCerrarSesion);
-		btnCerrarSesion.setFont(new Font("Footlight MT Light", Font.BOLD, 12));
 
 		JPanel panelBotones = new JPanel();
-		panelBotones.setLocation(655, 0);
-		panelBotones.setSize(131, 563);
+		panelBotones.setLocation(655, 70);
+		panelBotones.setSize(131, 493);
 		frmMain.getContentPane().add(panelBotones);
 		panelBotones.setLayout(new GridLayout(4, 1, 0, 0));
-
-		JButton btnPerfil = new JButton("");
-		btnPerfil.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		btnPerfil.setIcon(logoPerfil);
-		panelBotones.add(btnPerfil);
 
 		JButton btnConciertos = new JButton("CONCIERTOS");
 		btnConciertos.setFont(new Font("Footlight MT Light", Font.BOLD, 15));
@@ -194,6 +88,20 @@ public class MainWindow {
 		JButton btnNewButton_2 = new JButton("ENTRADAS");
 		btnNewButton_2.setFont(new Font("Footlight MT Light", Font.BOLD, 13));
 		panelBotones.add(btnNewButton_2);
+		
+		JButton btnPerfilPeque = new JButton("");
+		btnPerfilPeque.setBorder(null);
+		btnPerfilPeque.setBorderPainted(false);
+		btnPerfilPeque.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ProfileWindow pw = new ProfileWindow();
+				pw.initialize();
+				frmMain.dispose();
+			}
+		});
+		btnPerfilPeque.setIcon(logoPerfilPeque);
+		btnPerfilPeque.setBounds(694, 10, 50, 50);
+		frmMain.getContentPane().add(btnPerfilPeque);
 
 		frmMain.setVisible(true);
 	}
--- src/main/java/com/deustotickets/gui/ProfileWindow.java
+++ src/main/java/com/deustotickets/gui/ProfileWindow.java
@@ -1,5 +1,27 @@
 package com.deustotickets.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+import com.deustotickets.client.Resource;
+
 /**
  * 
  * @author BSPQ-03
@@ -7,4 +29,214 @@ package com.deustotickets.gui;
  */
 public class ProfileWindow {
 
+	private static JFrame frmProfile;
+	private static JTextField textFieldEmail;
+	private static JPasswordField passwordFieldContrasenya;
+	private static Border line_1 = BorderFactory.createLineBorder(new Color(194, 194, 194), 2);
+	private static Border line_2 = BorderFactory.createLineBorder(new Color(227, 30, 36), 2);
+	private static ImageIcon logo_1 = new ImageIcon("images/logoPerfil.png");
+	private static ImageIcon logo_2 = new ImageIcon("images/logoPerfil.png");
+	private static ImageIcon lapiz = new ImageIcon("images/pencil.png");
+
+	/**
+	 * @wbp.parser.entryPoint
+	 * 
+	 */
+	public static void initialize() {
+		frmProfile = new JFrame();
+		frmProfile.setTitle("DeustoTickets - Perfil " + MainWindow.logged.getNombreApellidos());
+		frmProfile.setBounds(100, 100, 400, 600);
+		frmProfile.setResizable(false);
+		frmProfile.setLocationRelativeTo(null);
+		frmProfile.setIconImage(logo_1.getImage());
+		frmProfile.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmProfile.getContentPane().setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(new Color(255, 255, 255));
+		frmProfile.getContentPane().add(panel);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 384, 214);
+		panel_1.setOpaque(false);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(logo_2);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(110, 24, 169, 180);
+		panel_1.add(lblNewLabel);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 213, 384, 191);
+		panel_2.setOpaque(false);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+
+		JLabel lblNombreUsuario = new JLabel("USUARIO:");
+		lblNombreUsuario.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNombreUsuario.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNombreUsuario.setBounds(40, 61, 117, 13);
+		panel_2.add(lblNombreUsuario);
+
+		JLabel lblMail = new JLabel("EMAIL:");
+		lblMail.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblMail.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblMail.setBounds(40, 89, 117, 13);
+		panel_2.add(lblMail);
+
+		JLabel lblContrasenya = new JLabel("CONTRASEÑA:");
+		lblContrasenya.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContrasenya.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblContrasenya.setBounds(40, 120, 117, 13);
+		panel_2.add(lblContrasenya);
+
+		JLabel lblNombreUsuario_1 = new JLabel("");
+		lblNombreUsuario_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNombreUsuario_1.setText(MainWindow.logged.getNombreApellidos());
+		lblNombreUsuario_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNombreUsuario_1.setBounds(184, 61, 117, 13);
+		panel_2.add(lblNombreUsuario_1);
+
+		JLabel lblMail_1 = new JLabel("");
+		lblMail_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblMail_1.setText(MainWindow.logged.getEmail());
+		lblMail_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMail_1.setBounds(184, 89, 117, 13);
+		panel_2.add(lblMail_1);
+
+		JLabel lblContrasenya_1 = new JLabel("");
+		lblContrasenya_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblContrasenya_1.setText(MainWindow.logged.getPassword());
+		lblContrasenya_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblContrasenya_1.setBounds(184, 120, 117, 13);
+		panel_2.add(lblContrasenya_1);
+
+		JButton btnLapiz = new JButton("");
+		btnLapiz.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Nombre de usuario antiguo: " + MainWindow.logged.getNombreApellidos());
+				changeUsername();
+				System.out.println("Nombre de usuario nuevo: " + MainWindow.logged.getNombreApellidos());
+				lblNombreUsuario_1.setText(MainWindow.logged.getNombreApellidos());
+			}
+		});
+		btnLapiz.setIcon(lapiz);
+		btnLapiz.setBorderPainted(false);
+		btnLapiz.setBounds(263, 43, 56, 31);
+		panel_2.add(btnLapiz);
+
+		JButton btnLapiz2 = new JButton("");
+		btnLapiz2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Contrasenya antigua: " + MainWindow.logged.getPassword());
+				changePassword();
+				System.out.println("Contrasenya nueva: " + MainWindow.logged.getPassword());
+				lblContrasenya_1.setText(MainWindow.logged.getPassword());
+			}
+		});
+		btnLapiz2.setIcon(lapiz);
+		btnLapiz2.setBorderPainted(false);
+		btnLapiz2.setBounds(263, 116, 56, 31);
+		panel_2.add(btnLapiz2);
+		
+		JLabel lblDatos = new JLabel("DATOS DE " + MainWindow.logged.getNombreApellidos() + ":");
+		lblDatos.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDatos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblDatos.setBounds(118, 10, 159, 13);
+		panel_2.add(lblDatos);
+
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(0, 403, 384, 156);
+		panel_3.setOpaque(false);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
+
+		JButton btnVolver = new JButton("VOLVER");
+		btnVolver.setBounds(85, 106, 200, 40);
+		btnVolver.setBackground(new Color(250, 209, 211));
+		btnVolver.setFocusPainted(false);
+		btnVolver.setOpaque(false);
+		btnVolver.setContentAreaFilled(false);
+		btnVolver.setBorder(line_1);
+		panel_3.add(btnVolver);
+
+		JButton btnBorrarCuenta = new JButton("BORRAR CUENTA");
+		btnBorrarCuenta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Resource.deleteAccount(MainWindow.logged.getEmail());
+				frmProfile.dispose();
+				LoginWindow lw = new LoginWindow();
+				lw.initialize();
+			}
+		});
+		btnBorrarCuenta.setOpaque(false);
+		btnBorrarCuenta.setFocusPainted(false);
+		btnBorrarCuenta.setContentAreaFilled(false);
+		btnBorrarCuenta.setBorder(line_1);
+		btnBorrarCuenta.setBackground(new Color(250, 209, 211));
+		btnBorrarCuenta.setBounds(85, 56, 200, 40);
+		panel_3.add(btnBorrarCuenta);
+
+		JButton btnCerrarSesion = new JButton("CERRAR SESIÓN");
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Resource.closeSession();
+				frmProfile.dispose();
+				LoginWindow lw = new LoginWindow();
+				lw.initialize();
+			}
+		});
+		btnCerrarSesion.setOpaque(false);
+		btnCerrarSesion.setFocusPainted(false);
+		btnCerrarSesion.setContentAreaFilled(false);
+		btnCerrarSesion.setBorder(line_1);
+		btnCerrarSesion.setBackground(new Color(250, 209, 211));
+		btnCerrarSesion.setBounds(85, 10, 200, 40);
+		panel_3.add(btnCerrarSesion);
+
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainWindow mw = new MainWindow();
+				mw.initialize();
+				frmProfile.dispose();
+			}
+		});
+
+		btnVolver.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent evt) {
+				btnVolver.setBorder(line_2);
+				btnVolver.setOpaque(true);
+			}
+
+			public void mouseExited(MouseEvent evt) {
+				btnVolver.setBorder(line_1);
+				btnVolver.setOpaque(false);
+			}
+		});
+
+		frmProfile.setVisible(true);
+	}
+
+	public static void changeUsername() {
+		String newUsername = JOptionPane.showInputDialog("Introduzca el nuevo nombre de usuario:");
+
+		if (newUsername != null) {
+			Resource.changeUsername(newUsername, MainWindow.logged.getEmail());
+			MainWindow.logged.setNombreApellidos(newUsername);
+			JOptionPane.showMessageDialog(null, "Nombre de usuario cambiado con éxito.");
+		}
+	}
+
+	public static void changePassword() {
+		String newPassword = JOptionPane.showInputDialog("Introduzca la nueva contraseña:");
+
+		if (newPassword != null) {
+			Resource.changePassword(MainWindow.logged.getEmail(), newPassword);
+			MainWindow.logged.setPassword(newPassword);
+			JOptionPane.showMessageDialog(null, "Contraseña cambiada con éxito.");
+		}
+	}
 }
